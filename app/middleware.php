b27e816e5c448befabcdcbe45d7c0b1c071cb59c
Michael Garifullin <michael.garifullin@smartbox.com>
2020-11-21 19:17:45 +0000

    addTwigMiddleware

--- app/middleware.php
+++ app/middleware.php
@@ -3,7 +3,14 @@ declare(strict_types=1);
 
 use App\Application\Middleware\SessionMiddleware;
 use Slim\App;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
-return function (App $app) {
+return static function (App $app) {
     $app->add(SessionMiddleware::class);
+
+    $app->add(TwigMiddleware::create(
+        $app,
+        Twig::create('src/templates/', ['cache' => 'var/cache/twig'])
+    ));
 };
--- app/routes.php
+++ app/routes.php
@@ -7,6 +7,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+use Slim\Views\Twig;
 
 return function (App $app) {
     $app->options('/{routes:.*}', function (Request $request, Response $response) {
@@ -22,5 +23,11 @@ return function (App $app) {
     $app->group('/users', function (Group $group) {
         $group->get('', ListUsersAction::class);
         $group->get('/{id}', ViewUserAction::class);
+    });
+
+    $app->get('/cart', function  (Request $request, Response $response) {
+        return Twig::fromRequest($request)->render($response, 'index.html', [
+            'name' => 'test'
+        ]);
     });
 };
